20538a5030b50e33b4ea3e6c598936de7c3de37d
Max Kofler <kofler.max.dev@gmail.com>
2022-04-25 21:30:53 +0200

    Added NamesDB::add() to add a new entry

--- include/namesDB.h
+++ include/namesDB.h
@@ -4,12 +4,10 @@
 #include <stdlib.h>			//size_t
 
 struct entry_namesDB{
-	struct entry_namesDB*		prev;
 	struct entry_namesDB*		next;
 	void*						entry;
 	uint8_t						nameLen;
-	char*						name;
-};
+} __attribute__((packed));
 
 class NamesDB{
 
@@ -35,6 +33,14 @@ public:
 	 */
 	void						clean();
 
+	/**
+	 * @brief	Adds the supplied string to the database
+	 * @param	str				The string to add
+	 * @param	entry			The entry to register
+	 * @return	size_t			The index of the string
+	 */
+	size_t						add(std::string str, void* entry);
+
 #ifndef FRIEND_NAMES_DB
 private:
 #endif
@@ -50,11 +56,26 @@ private:
 	size_t						_size_entries = 0;
 
 	/**
+	 * @brief	The bytes used of the entries block
+	 */
+	size_t						_used_bytes = 0;
+
+	/**
+	 * @brief	The amount of entries stored in this database
+	 */
+	size_t						_count_entries = 0;
+
+	/**
 	 * @brief	All the entries
 	 * This is the entry point into the names, this is NOT an array!
 	 * This is allocated in bytes, hence the uint8_t
 	 */
 	uint8_t*					_entries = nullptr;
+
+	/**
+	 * @brief	The last entry to fill in the next pointer
+	 */
+	entry_namesDB*				_last_entry = nullptr;
 
 };
 
---
+++ src/namesDB/add.cpp
@@ -0,0 +1,46 @@
+#include "log.h"
+#include "debug.h"
+
+#include "namesDB.h"
+
+size_t NamesDB::add(std::string str, void* entry_insert){
+	FUN();
+	DEBUG_EX("NamesDB::add()");
+
+	//If this assertion fails, the size of entry_namesDB has changed
+	//and this needs to be rewritten!
+	uint8_t size_struct = sizeof(entry_namesDB*) + sizeof(void*) + sizeof(uint8_t);
+	assert(sizeof(entry_namesDB) == size_struct);
+
+	//Not the amount of bytes needed for this entry
+	uint8_t size_entry = sizeof(entry_namesDB) + str.length();
+
+	//Allocate enough memory for this operation
+	while(_size_entries * _blockSize < _used_bytes + size_entry)
+		expand();
+
+	//Get a pointer to the begin of this new entry
+	entry_namesDB* entry = (entry_namesDB*)(_entries + _used_bytes);
+	
+	//Set this entry as the next entry for the previous entry
+	if (_last_entry != nullptr)
+		_last_entry->next = entry;
+
+	//Set the entries carried entry
+	entry->entry = entry_insert;
+
+	//Set the entry name length and copy the name
+	entry->nameLen = str.length();
+	uint8_t* nameStart = _entries + _used_bytes + sizeof(entry_namesDB);
+	std::memcpy(nameStart, str.c_str(), str.length());
+
+	//Update the database
+	_used_bytes += size_entry;
+	_last_entry = entry;
+
+	//Determine the id of this entry, add it and return it
+	size_t id = _count_entries;
+	_count_entries++;
+
+	return id;
+}
