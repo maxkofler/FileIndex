2682d663f7d2cac53a3af65b352528784b5f0503
Max Kofler <kofler.max.dev@gmail.com>
2022-04-25 21:41:49 +0200

    Moved tests with loads of output to a new _Stress test suite

--- tests/tests/NamesDB/src/tAdd.cpp
+++ tests/tests/NamesDB/src/tAdd.cpp
@@ -29,22 +29,6 @@ TEST(NamesDB, add_normal){
 	db.add("MyName", &db);
 }
 
-//Adds loads of names to the database
-TEST(NamesDB, add_loads_of_names){
-	FUN();
-
-	NamesDB db;
-
-	try{
-		for(size_t i = 0; i < 100000; i++)
-			db.add("Name" + std::to_string(i), nullptr);
-	} catch (std::bad_alloc& ba){
-		FAIL() << "Adding of loads of entries failed with bad_alloc exception: " + std::string(ba.what());
-	} catch (...) {
-		FAIL() << "Failed to add loads of entries with exception";
-	}
-}
-
 //Checks if the return value of add() is the correct id
 TEST(NamesDB, add_return){
 	FUN();
