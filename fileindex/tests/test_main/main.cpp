a58557496ae9628ca0a5a8b912f93a734911da2a
Max Kofler <kofler.max.dev@gmail.com>
2022-07-11 21:34:04 +0200

    Added profiling support from Log submodule

--- fileindex/tests/test_main/main.cpp
+++ fileindex/tests/test_main/main.cpp
@@ -8,6 +8,8 @@
 int glob_argc;
 char** glob_argv;
 
+#include <fstream>
+
 GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from %s\n", __FILE__);
   glob_argc = argc;
@@ -19,6 +21,7 @@ GTEST_API_ int main(int argc, char **argv) {
   hlog = new Log::Log(Log::MEM);
   hlog->setFeature(Log::FEATURE_PRINTFUNNAMES, false);
   hlog->setFeature(Log::FEATURE_PROFILE, true);
+<<<<<<< HEAD:fileindex/tests/test_main/main.cpp
   FUN();
 
   std::ofstream* outFile = new std::ofstream();
@@ -26,18 +29,36 @@ GTEST_API_ int main(int argc, char **argv) {
   hlog->setProfileStream(outFile);
 
   LOGU("Starting tests...");
+=======
+
+  std::ofstream outFile;
+  outFile.open("trace.json", std::ios::out);
+  hlog->setProfileStream(&outFile);
+>>>>>>> bb16c1d (Added profiling support from Log submodule):tests/test_main/main.cpp
 
   int res = 0;
-  try {
-    res = RUN_ALL_TESTS();
-  } catch (...) {
-    LOGUE("Other error");
+
+  {
+    FUN();
+
+    LOGU("Starting tests...");
+
+    try {
+      res = RUN_ALL_TESTS();
+    } catch (...) {
+      LOGUE("Other error");
+    }
   }
 
   delete hlog;
 
+<<<<<<< HEAD:fileindex/tests/test_main/main.cpp
   outFile->flush();
   outFile->close();
+=======
+  outFile.flush();
+  outFile.close();
+>>>>>>> bb16c1d (Added profiling support from Log submodule):tests/test_main/main.cpp
 
   return res;
 }
