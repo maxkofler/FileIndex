75be9effcb0856482c62bff48398c461b7a8fcba
Max Kofler <kofler.max.dev@gmail.com>
2022-07-11 22:49:04 +0200

    Fixed last errors due to fix of previous flaw in implementation

--- tests/tests/NamesDB/src/tSearchFirst.cpp
+++ tests/tests/NamesDB/src/tSearchFirst.cpp
@@ -27,16 +27,18 @@ TEST(NamesDB, searchFirst_find_name){
 	std::string name = "SomeRandomName";
 	std::string name2 = "SomeOtherRandomName2";
 
+	void* wrongpointer = (void*) &name;
+
 	NamesDB db;
 
 	size_t id = db.add(name, &db);
-	size_t id2 = db.add(name2, nullptr);
+	size_t id2 = db.add(name2, wrongpointer);
 
-	ASSERT_NE(nullptr, db.searchFirst(name));
+	ASSERT_NE(nullptr, db.searchFirst(name)) << "Could not find the entry";
 	ASSERT_EQ(&db, db.searchFirst(name)->entry);
 
-	ASSERT_NE(nullptr, db.searchFirst(name2));
-	ASSERT_EQ(nullptr, db.searchFirst(name2)->entry);
+	ASSERT_NE(nullptr, db.searchFirst(name2)) << "Could not find the entry";
+	ASSERT_EQ(wrongpointer, db.searchFirst(name2)->entry);
 }
 
 //Tests if searchFirst() finds a similar string, what it should not do
@@ -48,7 +50,9 @@ TEST(NamesDB, searchFirst_similar){
 
 	NamesDB db;
 
-	size_t idW = db.add(contentWrong, nullptr);
+	void* wrongpointer = (void*) &contentWrong;
+
+	size_t idW = db.add(contentWrong, wrongpointer);
 	size_t idT = db.add(contentTrue, &db);
 
 	{	//Check if the search find the wrong entry
@@ -62,7 +66,7 @@ TEST(NamesDB, searchFirst_similar){
 		entry_namesDB* res = db.searchFirst("myContent");
 
 		ASSERT_NE(nullptr, res) << "Could not find the required string";
-		EXPECT_EQ(nullptr, res->entry) << "Found the wrong item that was similar";
+		EXPECT_EQ(wrongpointer, res->entry) << "Found the wrong item that was similar";
 	}
 
 }
