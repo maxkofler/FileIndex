4811634d4df69fc7caf433b1a58def7e35fa8e70
Max Kofler <kofler.max.dev@gmail.com>
2022-09-12 21:51:46 +0200

    Readded functionality for adding fs entries

--- fileindex/src/fs/add.cpp
+++ fileindex/src/fs/add.cpp
@@ -10,14 +10,12 @@ size_t FS::add(const std::string& name, const fs_entry& entry){
     FUN();
     DEBUG_EX("FS::add()");
 
-    #warning This implementation is flawed! The crates should be stored in an array for size_t entries in the DB
     //Add the new entry
     _entries.add(entry);
 
     if (_useDirtyDB){
-        //_dirtyDB->add(name, (void*)(_entries.lastID()));
+        _dirtyDB->add(name, (void*)(_entries.lastID()));
     } else {
-        /*
         crate_s<size_t> newCrate;
         newCrate.data = nullptr;
         newCrate.size = 0;
@@ -27,7 +25,6 @@ size_t FS::add(const std::string& name, const fs_entry& entry){
         crate_add<size_t>(&_crates._data[_entries.lastID()], _entries.lastID());
 
         _db->add(name, (void*)(_crates.lastID()));
-        */
     }
 
     return _entries._size-1;
